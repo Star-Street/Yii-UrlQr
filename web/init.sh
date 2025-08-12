d3cacfad3dbda99c3a188786b2bd49120027e55a
AlexProgressive <lito1395@mail.ru>
2025-08-12 03:09:49 +0300

    initial: add init script

---
+++ web/init.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+cd /var/www/html || exit 1
+
+echo "### ######################"
+echo "### Installing composer dependencies..."
+if [ "$APP_ENV" = "prod" ]; then
+    composer install --no-dev --optimize-autoloader --no-interaction --no-progress
+else
+    composer install --optimize-autoloader --no-interaction --no-progress
+fi
+
+echo "### ######################"
+echo "### Setting permissions..."
+find . -type d -exec chmod 755 {} \;
+find . -type f -exec chmod 644 {} \;
+chown -R www-data:www-data .
+chmod +x yii
+
+echo "### ######################"
+echo "### Applying migrations..."
+php yii migrate --interactive=0
+
+echo "### End initial..."
+echo "### ######################"
